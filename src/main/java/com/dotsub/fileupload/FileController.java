7f1663ae6b8ed929f14eee8f0df16b7c78dd5dfd
Adolfo Vazquez <adolfo.software@gmail.com>
2019-07-21 10:16:55 -0500

    storing file in memory and hard drive

--- src/main/java/com/dotsub/fileupload/FileController.java
+++ src/main/java/com/dotsub/fileupload/FileController.java
@@ -1,22 +1,43 @@
 package com.dotsub.fileupload;
 
-import java.util.List;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200", allowCredentials = "true", allowedHeaders = "*")
 public class FileController {
- 
-    @Autowired
-    private FileRepository fileRepository;
-  
-    @PostMapping("/files")
-    void addFile(@RequestBody File file) {
-      fileRepository.save(file);
-    }
+
+  // Path were files will be stored
+  private static String STORAGE_PATH = "/tmp";
+
+  @Autowired
+  private FileRepository fileRepository;
+
+  @PostMapping("/files")
+  File addFile(@RequestParam("title") String title, @RequestParam("desc") String desc,
+      @RequestParam("date") String date, @RequestParam("file") MultipartFile file) throws IOException, ParseException {
+
+    Date creationDate = new SimpleDateFormat("dd/MM/yyyy").parse(date);  
+
+    File uploadedFile = new File(title, desc, creationDate);
+    fileRepository.save(uploadedFile);
+
+    Path filePath = Paths.get(STORAGE_PATH + "/" + file.getOriginalFilename());
+    Files.copy(file.getInputStream(), filePath, StandardCopyOption.REPLACE_EXISTING);
+
+    return uploadedFile;
+  }
 }
